930086cc569cfb07d10903df3bff77ff3f7654c1
riya lakhani <riyalakhani694@gmail.com>
2026-06-22 23:25:19 +0530

    -fix bugs

--- server/index.js
+++ server/index.js
@@ -10,12 +10,23 @@ dotenv.config();
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+      "http://localhost:3000",
+      "https://your-frontend-domain.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
